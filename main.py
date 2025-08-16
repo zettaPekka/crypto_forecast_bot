c90dff8d29ad622b1868362c7b8a32158d13c84a
Your Name <kigeandaviddd@gmail.com>
2025-08-16 10:34:09 +0300

    commit

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ async def run_bot():
     await dp.start_polling(bot)
 
 async def run_app():
-    config = uvicorn.Config('main:app')
+    config = uvicorn.Config('main:app', host='0.0.0.0', port=8000)
     server = uvicorn.Server(config)
     await server.serve()
 
